1926aa5dfb1e4e45e9adb15d713512415cbd6738
juan carlos fuentes <jcfuentest@EXPERIMAXs-MacBook-Pro-3.local>
2020-01-24 15:50:52 -0300

    ok

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/public"));
 hbs.registerPartials(__dirname + "/views/parciales");
 
 app.set('view engine', 'hbs');
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
@@ -28,6 +28,6 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.listen(3000, () => {
-    console.log('escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`escuchando peticiones en el puerto ${port}`);
 })
